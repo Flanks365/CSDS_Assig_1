50ffb15ea75c9f62f287c19873c615da97f2fd48
yuyue-creater <yyu129@my.bcit.ca>
2025-01-21 20:23:52 -0800

    added endpoints to get questions and answers plus foreign key attribute

--- Answers.cs
+++ Answers.cs
@@ -10,6 +10,7 @@ namespace CSDS_Assign_1
         public string AnswerText { get; private set; }
         public string IsCorrect { get; private set; }
         public string AnswerIndex { get; private set; }
+        public string FKAnswersQuestions { get; private set; }
         
 
         // Constructor to initialize properties
